d5fe0e44cfbbbe7255b42ad47f95598a44468a4e
TanYa-Go <tanja.godinic@gmail.com>
2021-03-21 19:46:02 +0000

    Add comments to app.js to describe functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@ const welcomeScreenRef = document.getElementById('welcome-screen');
 const gameScreenRef = document.getElementById('game-screen');
 const startGameRef = document.getElementById('start-btn');
 
-
+/* Function to show game screen and hide welcome screen on click */
 startGameRef.addEventListener('click', function() {
   welcomeScreenRef.classList.add('hidden');
   gameScreenRef.classList.remove('hidden');
 });
 
-
+/* Function to show difficulty level on button click */
 const dropdownsRef = document.getElementsByClassName("dropdown-content");
 const chooseLevelRef = document.getElementById ('drop');
 const levelChoice = document.getElementById('levelChoice');
