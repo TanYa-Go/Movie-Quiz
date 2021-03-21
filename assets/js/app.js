cdfd3b401465de78202e1c515de0679219f827ea
TanYa-Go <tanja.godinic@gmail.com>
2021-03-21 18:53:56 +0000

    Add dropdown function to the choose level button and edit button styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,17 @@ const welcomeScreenRef = document.getElementById('welcome-screen');
 const gameScreenRef = document.getElementById('game-screen');
 const startGameRef = document.getElementById('start-btn');
 
+
 startGameRef.addEventListener('click', function() {
   welcomeScreenRef.classList.add('hidden');
   gameScreenRef.classList.remove('hidden');
 });
 
+
+const dropdownsRef = document.getElementsByClassName("dropdown-content");
+const chooseLevelRef = document.getElementById ('drop');
+const levelChoice = document.getElementById('levelChoice');
+
+chooseLevelRef.addEventListener('click', function (){
+ levelChoice.classList.toggle("show");
+})
