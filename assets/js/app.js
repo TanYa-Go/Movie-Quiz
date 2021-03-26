cda399c25bf0251a1bc06f442e4db7eb29622b8d
TanYa-Go <tanja.godinic@gmail.com>
2021-03-26 13:11:38 +0000

    Fetch questions from API and display on the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,50 @@ startGameRef.addEventListener('click', function() {
 /* Function to show difficulty level on button click */
 const dropdownsRef = document.getElementsByClassName("dropdown-content");
 const chooseLevelRef = document.getElementById ('drop');
-const levelChoice = document.getElementById('levelChoice');
+const levelChoiceRef = document.getElementById('levelChoice');
 
 chooseLevelRef.addEventListener('click', function (){
- levelChoice.classList.toggle("show");
-})
+ levelChoiceRef.classList.toggle("show");
+});
+
+const questionRef = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('btn-answer'));
+console.log(questionRef );
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+ 
+//Fetch questions from API 
+fetch("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple")
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE NOT OK");
+        }
+    })
+    .then(function (data) {
+        console.log(data);
+        displayQuestions(data);
+    })
+    .catch((error) => {
+        console.error("FETCH ERROR:", error);
+    });
+
+// Function thay displays questions on the DOM
+function displayQuestions(data) {
+    const questions = data.results;
+    const questionRef = document.getElementById('question');
+
+    questions.forEach(questions => {
+        questionRef.innerHTML = questions.question
+    })
+
+}
