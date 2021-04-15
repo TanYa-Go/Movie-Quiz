16df1541f1f28671c8401e75ecef142901cb218c
TanYa-Go <tanja.godinic@gmail.com>
2021-04-15 18:03:02 +0000

    Add dynamic routing to go back to index page when Play Again button is clicked

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,6 +3,8 @@ document.getElementById('score').innerText = localStorage.mostRecentScore;
 const playAgainButtonRef = document.getElementById('play-again-btn');
 
 playAgainButtonRef.addEventListener('click', (e) => {
-   return window.location.assign('/index.html');
+const playAgainPath = window.location.protocol + "//" + window.location.host;
+return window.location.assign(playAgainPath);
 
 });
+
