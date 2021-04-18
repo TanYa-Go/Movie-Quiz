aec0e270eaecceea2dc2e1666ce3956abcb8bb3c
TanYa-Go <tanja.godinic@gmail.com>
2021-04-18 16:51:55 +0000

    Add different sounds to play with different scores at the end of the quiz

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,26 +1,27 @@
-/*const lowScoreSound = new Audio("assets/music/low-score.mp3");
+
+const lowScoreSound = new Audio("assets/music/low-score.mp3");
+const midScoreSound = new Audio("assets/music/mid-score.mp3")
 const highScoreSound = new Audio("assets/music/high-score.mp3");
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    if (score <= 50) {
-        lowScoreSound.play();
-    } else {
-        highScoreSound.play();
-    }
-  });
-  */
-
-
-
-
-document.getElementById('score').innerText = localStorage.mostRecentScore;
-
+const scoreRef = document.getElementById('score');
 const playAgainButtonRef = document.getElementById('play-again-btn');
+
+let endScore = scoreRef.innerText = localStorage.mostRecentScore;
+
+window.addEventListener('DOMContentLoaded', () => {
+      if (endScore <= 40) {
+    lowScoreSound.play();
+ } else if (endScore <=70) {
+    midScoreSound.play();
+ } else {
+   highScoreSound.play();
+}
+
+
 
 playAgainButtonRef.addEventListener('click', (e) => {
 const playAgainPath = window.location.protocol + "//" + window.location.host;
 return window.location.assign(playAgainPath);
 
-});
 
+});
+});
