647621839ad12fdd22ccd25d5d27c570dc3aa96c
TanYa-Go <tanja.godinic@gmail.com>
2021-04-20 16:13:23 +0000

    Add functionality to display different moive quotes depending on the user score

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,24 +4,47 @@ const midScoreSound = new Audio("assets/music/mid-score.mp3")
 const highScoreSound = new Audio("assets/music/high-score.mp3");
 const scoreRef = document.getElementById("score");
 const playAgainButtonRef = document.getElementById("play-again-btn");
-
+const movieQuoteRef = document.getElementById("movie-quote");
+const movieTitleRef = document.getElementById("movie-title");
 let endScore = scoreRef.innerText = localStorage.mostRecentScore;
+let quotesArray = []
 
-window.addEventListener("DOMContentLoaded", () => {
-      if (endScore <= 40) {
-    lowScoreSound.play();
- } else if (endScore <=70) {
-    midScoreSound.play();
- } else {
-   highScoreSound.play();
+async function getData() {
+   const response = await fetch("movieQuotes.json")
+      //.then(res => {
+      console.log(response);
+      const loadedQuotes = await response.json();
+      console.log(loadedQuotes);
+   //}).then(loadedQuotes => {
+      quotesArray = quotesArray.concat(loadedQuotes.movieQuotes);
+      console.log(quotesArray[2].quote);
+   //});
 }
+   window.addEventListener("DOMContentLoaded", async ()=> {
+      await getData()       
+      if (endScore <= 40) {
+         console.log(quotesArray);
+         movieQuoteRef.innerHTML = quotesArray[0].quote;
+         movieTitleRef.innerHTML = quotesArray[0].movie;
+         lowScoreSound.play();
+      } else if (endScore <=70) {
+         console.log(quotesArray);
+         movieQuoteRef.innerHTML = quotesArray[1].quote;
+         movieTitleRef.innerHTML = quotesArray[1].movie;
+         midScoreSound.play();
+      } else {
+         console.log(quotesArray);
+         movieQuoteRef.innerHTML = quotesArray[2].quote;
+         movieTitleRef.innerHTML = quotesArray[2].movie;
+         highScoreSound.play();
+      }
+   });
 
 
+   playAgainButtonRef.addEventListener("click", (e) => {
+         let playAgainPath = window.location.protocol + "//" + window.location.host + window.location.pathname;
+      playAgainPath = playAgainPath.replace('/end.html', '');
+      return window.location.assign(playAgainPath);
+   });
 
-playAgainButtonRef.addEventListener("click", (e) => {
-   let playAgainPath = window.location.protocol + "//" + window.location.host + window.location.pathname;
-   playAgainPath = playAgainPath.replace('/end.html', '');
-   return window.location.assign(playAgainPath);
- });
 
-})
